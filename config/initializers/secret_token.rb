a601e7c8c72004db26d108160585695a1607ca27
root <root@rails.(none)>
2013-05-28 13:59:01 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testap::Application.config.secret_token = '13d4d4a97274e97a064ac80143c8274c07bcd62aa5df5f75dca34790dad4d44380eb081863b44cd2d6d0c2e0eacd0361f0e1320a5c747bd321977d3f81ac7199'
